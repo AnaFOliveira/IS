1a1e145f9a0b127fd48d54101307c8d80a67182e
Nelson Monteiro <nelsonrcm117@gmail.com>
2018-04-09 15:50:25 +0100

    Project 2

---
+++ project_2/src/project_2/Loader.java
@@ -0,0 +1,38 @@
+package project_2;
+
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+
+
+
+public class Loader {
+	protected static Catalog catalog = Unmarshall.unmarshall("Projeto1_xml");
+	protected static List<Vinyl> list_vinyl = catalog.getVinyl();
+	public static void main(String[] args){
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("project_2");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		for (Vinyl v : list_vinyl)
+			for (Music music: v.getMusic())
+				em.persist(music);
+		tx.commit();
+		tx.begin();
+		for (Vinyl v : list_vinyl)
+			em.persist(v);
+		tx.commit();
+		
+		// after commit we have ids:
+		// Close an application-managed entity manager.
+		em.close();
+		// Close the factory, releasing any resources that it holds.
+		emf.close();
+	}
+
+}
